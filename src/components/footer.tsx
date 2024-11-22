bf93f8c8a045b285043a712895b704e2ee43f2d4
Pau Kraft <beastmodepau@gmail.com>
2024-11-22 19:03:59 +0100

    ✨ 1.0

---
+++ src/components/footer.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer className="w-full flex-1 flex flex-col justify-end">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0 py-6 px-6">
+        <p className="text-xs text-muted-foreground">by Pau Kraft</p>
+        <div className="flex flex-wrap items-center justify-center gap-3">
+          <Link href="https://twitter.com/paukraft" target="_blank">
+            <p className="text-sm text-muted-foreground hover:underline">
+              Twitter / X
+            </p>
+          </Link>
+          <Link
+            href="https://bsky.app/profile/paukraft.bsky.social"
+            target="_blank"
+          >
+            <p className="text-sm text-muted-foreground hover:underline">
+              Bluesky
+            </p>
+          </Link>
+          <Link href="/imprint">
+            <p className="text-sm text-muted-foreground hover:underline">
+              Imprint
+            </p>
+          </Link>
+          <Link href="/privacy">
+            <p className="text-sm text-muted-foreground hover:underline">
+              Privacy
+            </p>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
